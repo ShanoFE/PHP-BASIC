8b2abcea8a1b5fd694d35beca7e77c4619ff8275
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-05-20 11:38:10 +0200

    Klaar

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,13 @@
+<?php
+$test1 = 12;
+$test2 ="tekst";
+
+echo var_dump($test1 == $test2);
+echo var_dump($test1 === $test2);
+echo var_dump($test1 != $test2);
+echo var_dump($test1 > $test2);
+echo var_dump($test1 < $test2);
+echo var_dump($test1 <> $test2);
+echo var_dump($test1 >= $test2);
+echo var_dump($test1 <= $test2);
+?>
