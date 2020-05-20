2ff67ae92de1a5067d5c88b00d3c1e95cb00704e
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-05-20 11:11:33 +0200

    klaar

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$myMovies = array("Spiderman", "Batman", "The Joker");
+echo "<ul>$myMovies[0]</ul>";
+echo "<br>";
+echo "<ul>$myMovies[1]</ul>";
+echo "<br>";
+echo "<ul>$myMovies[2]</ul>";
+?>
