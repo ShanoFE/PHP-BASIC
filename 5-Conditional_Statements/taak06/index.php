8015ce1f5290bfd73ddf7d4913d3ab2ac9a811ac
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-05-20 12:07:11 +0200

    Klaar

---
+++ 5-Conditional_Statements/taak06/index.php
@@ -0,0 +1,25 @@
+<?php
+$test1 = 6;
+$test2 =10;
+
+if ($test1 == $test2) {
+    echo "gelijk";
+} else {
+    echo "ongelijk";
+}
+if ($test1 != $test2) {
+    echo "gelijk";
+} else {
+    echo "ongelijk";
+}
+if ($test1 > $test2) {
+    echo "test1 is groter dan test2";
+} else {
+    echo "ongelijk";
+}
+if ($test1 < $test2) {
+    echo "test1 is kleiner dan test2";
+} else {
+    echo "ongelijk";
+}
+?>
