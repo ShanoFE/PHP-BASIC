3cf551778eefeea3b429609c780f4ae2b5cf46a9
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-05-20 12:28:23 +0200

    Klaar

---
+++ 7-Include-Require/includes/menu.php
@@ -0,0 +1,8 @@
+<?php include 'menu.php';
+$menu = array("menu-item 1", "menu-item 2", "menu-item 3");
+echo "<ul>$menu[0]</ul>";
+echo "<br>";
+echo "<ul>$menu[1]</ul>";
+echo "<br>";
+echo "<ul>$menu[2]</ul>";
+?>
